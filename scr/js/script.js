0310f1a8b9feca3f30e7e18accc7e43c95356323
Broccoli <zeratukin@gmail.com>
2022-01-30 20:51:18 +1000

    media requsts

--- scr/js/script.js
+++ scr/js/script.js
@@ -3,11 +3,10 @@ let splide = new Splide('.splide', {
     perPage: 2,
     perMove: 1,
     rewind: true,
-    width: '1170',
     gap: '30px',
     pagination: false,
     grag: true,
-    wheel: true
+    wheel: true,
 });
 
 splide.mount();
